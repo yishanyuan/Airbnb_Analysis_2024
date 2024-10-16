9fe7e57083536a682f90cae61e87b87d3aee8a79
Max <143114799+maxyep1@users.noreply.github.com>
2024-10-16 15:57:40 -0500

    update

--- code/manipulated_variables.py
+++ code/manipulated_variables.py
@@ -2,13 +2,12 @@ import pandas as pd
 import os
 
 # Define the base directory for your project (moving up one level to the project root directory)
-base_dir = os.path.dirname(os.path.dirname(__file__))  # 上一级目录
+base_dir = os.path.dirname(os.path.dirname(__file__)) 
 
 # Define relative paths for input CSV and output CSV (located in 'data' folder)
 data_folder_path = os.path.join(base_dir, 'data')
 csv_file_path = os.path.join(data_folder_path, 'output_data.csv')  
-updated_csv_file_path = os.path.join(data_folder_path, 'variables_output_data.csv')  # 生成后的CSV文件名
-
+updated_csv_file_path = os.path.join(data_folder_path, 'variables_output_data.csv')  
 # Load the CSV file into a DataFrame
 df = pd.read_csv(csv_file_path)
 
--- code/split_data.py
+++ code/split_data.py
@@ -1,15 +1,12 @@
 import pandas as pd
 
-# 读取数据文件，使用相对路径
 file_path = './data/updated_output_data.xlsx'
 data = pd.read_excel(file_path)
 
-# 根据租期长度过滤数据
 one_day = data[data['Length of lease'] == 'one day']
 one_week = data[data['Length of lease'] == 'one week']
 one_month = data[data['Length of lease'] == 'one month']
 
-# 保存每个子集到相应的文件中，使用相对路径
 one_day_path = './data/one_day.xlsx'
 one_week_path = './data/one_week.xlsx'
 one_month_path = './data/one_month.xlsx'
